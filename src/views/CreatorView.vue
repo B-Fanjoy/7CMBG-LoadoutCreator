b70b081006a4a9474b593db303f4a6c14aee6ae5
B-Fanjoy <fanjoyben@hotmail.com>
2025-02-03 12:19:33 -0400

    working on adding loadout creator page

---
+++ src/views/CreatorView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Loadout Creator</h1>
+  <div class="flex space-x-2">
+    <h2>Weapons</h2>
+    <h2>Weapons2</h2>
+    <h2>Weapons3</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreatorView',
+}
+</script>
