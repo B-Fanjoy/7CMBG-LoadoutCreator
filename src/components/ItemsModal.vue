43dc1788b75392e4813ea9a58d0dfd16ed59c7e9
B-Fanjoy <befanjoy@gmail.com>
2025-03-24 11:31:35 -0300

    search working

--- src/components/ItemsModal.vue
+++ src/components/ItemsModal.vue
@@ -1,16 +1,20 @@
 <template>
   <teleport to="body">
     <div class="fixed top-0 left-0 w-full h-full bg-[#00000085] flex justify-center items-center" v-if="isOpen">
-
       <!-- Modal Container -->
       <div class="flex flex-col bg-[#4d4d4d] border-2 border-[#F4C356] rounded-2xl w-auto h-auto">
-
         <!-- Modal Header -->
         <div class="relative flex justify-center items-center border-b-2 pb-1 rounded-t-2xl text-[#F4C356] bg-[#2e2e2e]">
           <h2 class="text-4xl font-semibold py-2 text-center">
             Add an Item
           </h2>
-          <input type="text" class="absolute right-2 top-2 p-2 text-center rounded-2xl bg-[#585858] border-2 border-[#F4C356] text-[#F4C356]" placeholder="Search" />
+          <input
+            type="text"
+            v-model="searchQuery"
+            @focus="clearSection"
+            class="absolute right-2 top-2 p-2 text-center rounded-2xl bg-[#585858] border-2 border-[#F4C356] text-[#F4C356]"
+            placeholder="Search"
+          />
         </div>
 
         <!-- Section Selector -->
@@ -23,20 +27,12 @@
           </div>
         </div>
 
-        <!-- Item Selection (Auto-fill Groups in Row) -->
-        <div v-if="selectedSection" class="flex flex-grow overflow-hidden w-full">
-
-          <div v-for="group in filteredItems" :key="group.id"
-            class="flex flex-col flex-1 border-1 border-[#F4C356] min-w-fit w-auto max-w-full">
-
-            <!-- Group Header -->
-            <h2 class="text-2xl font-semibold text-center border-b-2 text-[#F4C356] bg-[#2e2e2e] p-2">
-              {{ group.name }}
-            </h2>
-
-            <!-- Scrollable Group List -->
+        <!-- Item Selection -->
+        <div class="flex flex-grow overflow-hidden w-full">
+          <div class="flex flex-col flex-1 border-1 border-[#F4C356] min-w-fit w-auto max-w-full">
+            <!-- Scrollable List -->
             <div class="flex-1 overflow-y-auto max-h-[40vh] text-center custom-scrollbar">
-              <div v-for="item in group.options" :key="item.id"
+              <div v-for="item in filteredItems" :key="item.id"
                   @click="selectItem(item.id)"
                   class="grid grid-cols-1 gap-2 bg-[#585858] hover:bg-[#3b3b3b] cursor-pointer p-2 items-center text-center not-last:border-b-2 border-[#F4C356] border-dashed">
                 <!-- Item Name -->
@@ -45,16 +41,13 @@
                 </div>
               </div>
             </div>
-
           </div>
-
         </div>
 
         <!-- Close Button -->
         <button class="border-t-2 border-[#F4C356] rounded-b-2xl bg-[#634545] hover:bg-[#673b3b] p-2 mt-auto" @click="close">
           Close
         </button>
-
       </div>
     </div>
   </teleport>
@@ -78,8 +71,12 @@ export default {
     },
   },
   methods: {
+    clearSection() {
+      this.selectedSectionId = null;
+    },
     close() {
       this.$emit('update:isOpen', false);
+      this.searchQuery = "";
       this.selectedSectionId = null;
     },
     selectSection(sectionId) {
@@ -91,16 +88,26 @@ export default {
     }
   },
   computed: {
-    selectedSection() {
-      return this.sections.find(sec => sec.id === this.selectedSectionId) || null;
-    },
     filteredItems() {
-      return this.selectedSection ? (this.selectedSection.groupings || []) : [];
+      let items = this.sections.flatMap(section =>
+        section.groupings.flatMap(group =>
+          group.options.map(option => ({ ...option, sectionId: section.id }))
+        )
+      );
+
+      if (this.selectedSectionId) {
+        items = items.filter(item => item.sectionId === this.selectedSectionId);
+      }
+
+      return items.filter(item =>
+        item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
     }
   },
   data() {
     return {
       sections: itemsData.sections,
+      searchQuery: "",
       selectedSectionId: null
     }
   }
